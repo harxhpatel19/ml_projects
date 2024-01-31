7f5791fe48fcc39dc350fac8094b9959a9d8a800
harxhpatel19 <158267449+harxhpatel19@users.noreply.github.com>
2024-01-31 15:18:31 +0530

    Add files via upload

--- heart_disease_ml.py
+++ heart_disease_ml.py
@@ -1,6 +1,7 @@
 import pickle
 import streamlit as st
 import os
+import numpy as np
 
 model_path = 'https://github.com/harxhpatel19/ml_projects/blob/main/heart_disease_data.csv'
 
@@ -19,50 +20,30 @@ if selected == 'Heart Disease Prediction':
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        age = st.text_input('Age')
+        age = st.text_input('Age', type='number')
 
     with col2:
-        sex = st.text_input('Sex')
+        sex = st.selectbox('Sex', ['Male', 'Female'])
+
+    # Convert sex to numeric representation
+    sex_mapping = {'Male': 1, 'Female': 0}
+    sex = sex_mapping[sex]
 
     with col3:
-        cp = st.text_input('Chest Pain types')
+        cp = st.text_input('Chest Pain types', type='number')
 
     with col1:
-        trestbps = st.text_input('Resting Blood Pressure')
+        trestbps = st.text_input('Resting Blood Pressure', type='number')
 
-    with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl')
-
-    with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
-
-    with col1:
-        restecg = st.text_input('Resting Electrocardiographic results')
-
-    with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved')
-
-    with col3:
-        exang = st.text_input('Exercise Induced Angina')
-
-    with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
-
-    with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment')
-
-    with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
-
-    with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+    # Repeat the same process for other input features...
 
     # code for Prediction
     heart_diagnosis = ''
 
     # creating a button for Prediction
     if st.button('Heart Disease Test Result'):
-        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+        input_data = np.array([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+        heart_prediction = heart_disease_model.predict(input_data)
 
         if heart_prediction[0] == 1:
             heart_diagnosis = 'The person is having heart disease'
@@ -70,3 +51,4 @@ if selected == 'Heart Disease Prediction':
             heart_diagnosis = 'The person does not have any heart disease'
 
     st.success(heart_diagnosis)
+
